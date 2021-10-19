417413928f13215a94adf5e43fb4a674fbfa298c
Stanislav Derkach <Sderkach1987@gmail.com>
2021-10-19 20:02:06 +0300

    init commit

--- src/com/pb/derkach/hw3/Array.java
+++ src/com/pb/derkach/hw3/Array.java
@@ -2,7 +2,7 @@ package com.pb.derkach.hw3;
 
 import java.util.Scanner;
 
-public class BubbleSort {
+public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Создайте массив данных в количестве 10 элементов, а мы попробуем их отсортировать от меньшего к большему.");
@@ -17,8 +17,26 @@ public class BubbleSort {
         for (int anArray : array) {
             System.out.print(anArray + " ");
         }
+// Отсупаем и считаем сумму элементов массива путем перебора и добавения в новую переменную
+        System.out.println();
+        int sumElem = 0;
+for (int і = 0; і < array.length; і++) {
+    sumElem = sumElem + array[і];
+}
+        System.out.println();
+        System.out.println("Сумма элементов введенного массива: " + sumElem);
+// Считаем количество положительных элементов массива путем перебора и увеличения каунтера
+        int counter = 0;
 
-        bubbleSort(array);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0)
+            counter += 1;
+
+        }
+        System.out.println();
+        System.out.println("Количество положительных элементов введенного массива: " + counter);
+
+       bubbleSort(array);
 // Выводим пользователю отсортированный массив
         System.out.println();
         System.out.println("Получите Ваш отсортированный массив от меньшего к большему:");
